7831efe8d28b589d666ae5282666eabe40b0b16c
Ryder Bray <112464374+RyderBray@users.noreply.github.com>
2023-07-14 15:13:36 -0400

    Initial Commit
    Simple calculator created with HTML, CSS, and JavaScript.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Calculator App
- Calculator app created using JavaScript
+7/14/23 1.0: Calculator app created using JavaScript, HTML, and CSS
---
+++ calculator.js
@@ -0,0 +1,101 @@
+const display = document.getElementById("numDisplay");
+const buttons = document.querySelectorAll('button');
+let clickedValues = [];
+let equation = [];
+let lastInput = null
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        const buttonClick = button.innerHTML;
+        const Operators = ["%", "⌫", "c", "=", "x", "÷", "+", "-"]
+        const lastInputType = getLastInputType(equation);
+
+    if (buttonClick === lastInput){
+        return;
+    }    
+    else if (Operators.includes(buttonClick)) { //Checks for operators
+        if (buttonClick === "%") {
+            percent();
+        }
+        else if (buttonClick === "⌫") {
+            equation.pop();
+            clickedValues.pop();
+            display.textContent = clickedValues.join("");
+        }
+        else if (buttonClick === "c") {
+            cancel();
+        }
+        else if (buttonClick === "=") {
+            equals();
+        }
+        else if (buttonClick === "÷") {
+            if (lastInputType === "number") {
+                displayOperators("/");
+            }
+        }
+        else if (buttonClick === "x") {
+            if (lastInputType === "number") {
+                displayOperators("*");
+            }
+        }
+        else if (buttonClick === "+") {
+            if (lastInputType === "number") {
+                displayOperators("+");
+            }
+        }
+        else if (buttonClick === "-") {
+            if (lastInputType === "number") {
+                displayOperators("-");
+            }
+        }
+    }
+    else { //Adds numbers to equation and display
+        clickedValues.push(buttonClick);
+        equation.push(buttonClick);
+        display.textContent = clickedValues.join("");
+    }
+    });
+});
+
+function getLastInputType(equation) {
+    if (equation.length === 0) {
+      return "none";
+    }
+    const lastItem = equation[equation.length - 1];
+    if (!isNaN(parseFloat(lastItem))) {
+      return "number";
+    }
+    return "operator";
+  }
+
+function percent() {
+    const currentNumber = parseFloat(clickedValues.join(""));
+    const percentage = currentNumber / 100;
+    clickedValues = [percentage.toString()];
+    equation = [percentage.toString()];
+    equation.push("*");
+    clickedValues.push("*");
+    display.textContent = clickedValues.join("");
+}
+
+function displayOperators(sign) {
+    equation.push(sign);
+    clickedValues.push(sign);
+    display.textContent = clickedValues.join("");
+}
+
+function equals() {
+    let finalEquation = equation.join("");
+    result = eval(finalEquation);
+    equation = [result];
+    lastInput = null;
+    clickedValues = [result];
+    display.textContent = result;
+}
+
+function cancel() {
+    clickedValues = [];
+    equation = [];
+    display.textContent = "";
+    lastInput = null;
+}
